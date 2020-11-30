d1c643a4950f2d83dcafb8da713a2624bc284f18
Ceci <ceci.herbert@hotmail.com>
2020-11-30 20:59:00 -0300

    agrego mailing

---
+++ laboBA_03.R
@@ -0,0 +1,39 @@
+library(googlesheets4)
+library(gmailr)
+library(tidyverse)
+
+# if didn't configure before
+gm_auth_configure(path = "gmailr_credentials.json")
+
+
+#df <- read_csv("lista.csv")
+df <- read_sheet("https://docs.google.com/spreadsheets/d/13TJs8gvHN6MZBUcLy49A_ujiCcf5ZlaZeJbiZECupwQ/edit#gid=173808074")
+#df <- df[2:nrow(df),] #cuando sin querer le mandaste ya al primero
+txt <- read_tsv("laboBA_03.txt",
+                col_names = FALSE) %>% 
+  unlist(use.names = FALSE)
+# collapse to a length 1 vector
+txt <- paste0(txt, collapse = "\n")
+
+msgs <- list()
+for (i in seq_along(1:nrow(df))) {
+  
+  m <- paste0("¡Hola ", df$Nombre[i], "!")
+ # m <- paste0("¡Hola!")
+  m <- paste0(m, txt)
+  
+  msgs[[i]] <- gm_mime() %>%
+    #gm_to(df$EmailAddress[i]) %>%
+    gm_to(df$Mail[i]) %>%
+    gm_from("talleresopensource@gmail.com") %>%
+    gm_html_body(body = m) %>%
+    gm_subject("Asesoramiento en comunidad con BA Lab")
+}
+
+# explore text
+gm_create_draft(msgs[[1]])
+
+
+# send the messages
+#lapply(msgs,
+#       function(x) send_message(x))
